90602fd773764baa3e68d046ca577f2f03111c52
Antonio Ruiz <luneth1314@gmail.com>
2021-12-14 02:48:02 -0500

    Memoized the minimax algorithm.

--- ai.py
+++ ai.py
@@ -7,11 +7,30 @@ from board import Board
 
 
 class AI:
-    def __init__(self, player_id, difficulty, num_players, winning_row_length):
+    def __init__(
+        self,
+        player_id,
+        difficulty,
+        num_rows,
+        num_columns,
+        num_players,
+        winning_row_length,
+    ):
         self.difficulty = difficulty
         self.player_id = player_id
         self.num_players = num_players
         self.winning_row_length = winning_row_length
+
+        def num_possible_states(num_rows, num_columns, num_players):
+            res = 0
+            for i in range(num_columns * num_rows):
+                res += num_players * ((num_players + 1) ** i)
+            return res
+
+        self.num_possible_states = num_possible_states(num_rows, num_columns, num_players)
+        self.state_scores = [
+            None for _ in range(self.num_possible_states*2)
+        ]
 
     def get_move(self, board, ui):
         if self.difficulty == 1:
@@ -50,20 +69,32 @@ class AI:
         return board.unhash(max(range(len(scores)), key=scores.__getitem__))
 
     def score_state(self, board, prev_player, depth=1, alpha=-inf, beta=inf):
-        if board.get_player_score(prev_player) >= self.winning_row_length:
+        state_hash = board.hash_state()
+        state_score = self.state_scores[state_hash]
+        if state_score is not None:
+            return state_score
+
+        if board.get_player_score(prev_player) == self.winning_row_length:
+            res = 1 / depth if prev_player == self.player_id else -1 / depth
+            self.state_scores[state_hash + self.num_possible_states] = res
             return 1 / depth if prev_player == self.player_id else -1 / depth
         elif board.has_no_blanks():
+            self.state_scores[state_hash] = 0
             return 0
 
         current_player = max((prev_player + 1) % (self.num_players + 1), 1)
         if current_player == self.player_id:
             optimal_score = -inf
             for x, y in product(range(board.num_rows), range(board.num_columns)):
-                if board.is_point_marked([x, y]):
+                if board.is_point_marked((x, y)):
                     continue
-                state = board.from_board(board)
-                state.add_mark(x, y, current_player)
-                score = self.score_state(state, current_player, depth + 1, alpha, beta)
+                temp = board.from_board(board)
+                temp.add_mark(x, y, current_player)
+                state_hash = temp.hash_state()
+                score = self.score_state(
+                    temp, current_player, depth + 1, alpha, beta
+                )
+                self.state_scores[state_hash] = score
                 optimal_score = max(optimal_score, score)
                 alpha = max(score, optimal_score)
                 if beta <= alpha:
@@ -76,7 +107,11 @@ class AI:
                     continue
                 temp = board.from_board(board)
                 temp.add_mark(x, y, current_player)
-                score = self.score_state(temp, current_player, depth + 1, alpha, beta)
+                state_hash = temp.hash_state() + self.num_possible_states
+                score = self.score_state(
+                    temp, current_player, depth + 1, alpha, beta
+                )
+                self.state_scores[state_hash] = score
                 optimal_score = min(optimal_score, score)
                 beta = min(score, optimal_score)
                 if beta <= alpha:
--- run_tests.py
+++ run_tests.py
@@ -1,32 +1,40 @@
 import sys
 import unittest
 
-from main import *
+from game import *
 from board import Board
 
+
 class BoardTests(unittest.TestCase):
-    def test_add_mark(self):
-        board = Board(num_rows=3, num_columns=3, num_players=1)
-        board.add_mark(0, 0, player_id=1)
-        self.assertEqual(board.board, [[1, 0, 0], [0, 0, 0], [0, 0, 0]])
-        board.add_mark(0, 1, player_id=1)
-        self.assertEqual(board.board, [[1, 1, 0], [0, 0, 0], [0, 0, 0]])
-        board.add_mark(0, 2, player_id=1)
-        self.assertEqual(board.board, [[1, 1, 1], [0, 0, 0], [0, 0, 0]])
+    # def test_add_mark(self):
+    #     board = Board(num_rows=3, num_columns=3, num_players=1)
+    #     board.add_mark(0, 0, player_id=1)
+    #     self.assertEqual(board.board, [[1, 0, 0], [0, 0, 0], [0, 0, 0]])
+    #     board.add_mark(0, 1, player_id=1)
+    #     self.assertEqual(board.board, [[1, 1, 0], [0, 0, 0], [0, 0, 0]])
+    #     board.add_mark(0, 2, player_id=1)
+    #     self.assertEqual(board.board, [[1, 1, 1], [0, 0, 0], [0, 0, 0]])
+
+    def test_hash_state(self):
+        board = Board(num_rows=3, num_columns=3, num_players=2)
+        board.board = [1 for _ in range(board.num_rows * board.num_columns)]
+        self.assertEqual(board.board, board.unhash_state(board.hash_state()))
+        board.board = [0 for _ in range(board.num_rows * board.num_columns)]
+        self.assertEqual(board.board, board.unhash_state(board.hash_state()))
+        board.board = [1, 0, 1, 0, 1, 0, 1, 0, 1]
+        self.assertEqual(board.board, board.unhash_state(board.hash_state()))
 
     def test_get_player_score(self):
         board = Board(num_rows=3, num_columns=3, num_players=2)
         board.add_mark(0, 0, 1)
         board.add_mark(1, 0, 1)
         board.add_mark(2, 0, 1)
-        self.assertEqual(board.board, [[1, 0, 0], [1, 0, 0], [1, 0, 0]])
         self.assertEqual(board.get_player_score(1), 3)
         self.assertEqual(board.has_no_blanks(), False)
         board = Board(num_rows=3, num_columns=3, num_players=2)
         board.add_mark(0, 0, 1)
         board.add_mark(0, 2, 1)
         board.add_mark(1, 1, 1)
-        self.assertEqual(board.board, [[1, 0, 1], [0, 1, 0], [0, 0, 0]])
         self.assertEqual(board.get_player_score(1), 2)
         self.assertEqual(board.has_no_blanks(), False)
         board.add_mark(0, 1, 2)
@@ -35,7 +43,6 @@ class BoardTests(unittest.TestCase):
         board.add_mark(2, 0, 2)
         board.add_mark(2, 1, 1)
         board.add_mark(2, 2, 2)
-        self.assertEqual(board.board, [[1, 2, 1], [2, 1, 2], [2, 1, 2]])
         self.assertEqual(board.get_player_score(2), 2)
         self.assertEqual(board.get_player_score(1), 2)
         self.assertEqual(board.has_no_blanks(), True)
@@ -46,11 +53,9 @@ class BoardTests(unittest.TestCase):
         board.add_mark(1, 2, 1)
         board.add_mark(2, 1, 1)
         board.add_mark(2, 2, 1)
-        self.assertEqual(board.board, [[1, 1, 0], [1, 0, 1], [0, 1, 1]])
         self.assertEqual(board.get_player_score(1), 2)
         self.assertEqual(board.has_no_blanks(), False)
         board.add_mark(1, 1, 1)
-        self.assertEqual(board.board, [[1, 1, 0], [1, 1, 1], [0, 1, 1]])
         self.assertEqual(board.get_player_score(1), 3)
         self.assertEqual(board.has_no_blanks(), False)
 
