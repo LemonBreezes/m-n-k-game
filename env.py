e47c0d4ad0286b101320529ecb1400c38c987d6f
Antonio Ruiz <luneth1314@gmail.com>
2021-12-23 19:36:52 -0500

    Removed env.py and documented main.py

--- env.py
+++
@@ -1,9 +0,0 @@
-"""The default and environment variables for this project"""
-
-# Constants
-NUM_ROWS=4
-NUM_COLUMNS=4
-WINNING_ROW_LENGTH=3
-IS_HUMAN_PLAYING=True
-GRAPHIC=False
-OPENING_PLAYER='random'
