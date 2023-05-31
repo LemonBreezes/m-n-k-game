6c13597859e7279cff7eaa570bf623d6dfdfd9ff
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:15:15 -0400

    Added a dumb AI

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import sys
 from itertools import product
 from numpy import matrix
 from math import floor
+from time import sleep
+from random import randrange
 
 # constants
 board_size = 3
-num_players = 1
+num_players = 2
 winning_row_length = 3
 
 # exception classes
@@ -31,12 +33,24 @@ class TicTacToeBoard(object):
         
     def getBoard(self):
         return self._board
+    
+    def getSize(self):
+        return self._size
+
+    def getWinningRowLength(self):
+        return self._winning_row_length
+    
+    def getNumPlayers(self):
+        return self._num_players
 
     def addMark(self, x, y, player):
         x = x - 1
         y = y - 1
         board = self._board
         board[x][y] = player
+        
+    def isPointMarked(self,x,y):
+        return not (self._isPointInBoard(x,y) and self._board[x][y] == 0)
 
     def getWinner(self):
         num_aligned = 1
@@ -83,19 +97,48 @@ class TicTacToeBoard(object):
     def clear(self):
         self._board = [[0 for i in range(self.size)] for j in range(self.size)]
 
+class TicTacToeAI(object):
+    def __init__(self, board, player_id, paradigm):
+        self._board = board
+        self._paradigm = paradigm
+        self._id = player_id
+        pass
+
+    def makeNextMove(self):
+        if (self._paradigm == 'random'):
+            self.makeRandomMove()
+
+    def makeRandomMove(self):
+        while (True):
+            x = randrange(self._board.getSize())
+            y = randrange(self._board.getSize())
+            if (self._board.getBoard()[x][y] == 0):
+                self._board.addMark(x+1,y+1,self._id)
+                break
+
 class TicTacToeGame(object):
     def __init__(self, size, num_players, winning_row_length):
-        self.board = TicTacToeBoard(size, num_players, winning_row_length)
+        self._board = TicTacToeBoard(size, num_players, winning_row_length)
 
     def start(self):
-        board = self.board
+        board = self._board
+        AI = TicTacToeAI(board=board, player_id=2, paradigm='random')
         board.display()
         while True:
             move = [ int(x) for x in input('Make your move: ').split()]
+            if (board.isPointMarked(move[0]-1,move[1]-1)):
+                print('Your choice of move is invalid.')
+                continue
             board.addMark(x=move[0],y=move[1],player=1)
             board.display()
             if (board.getWinner() == 1):
                 print('You win!')
+                break
+            sleep(2)
+            AI.makeNextMove()
+            board.display()
+            if (board.getWinner() > 1):
+                print('You lost!')
                 break
 
 # internal functions & classes
