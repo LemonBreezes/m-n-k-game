33dd5706870b9c4529f565f9ae13423bc03f0d70
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:15:15 -0400

    Redid everything

---
+++ ai.py
@@ -0,0 +1,62 @@
+from random import randrange
+from functools import lru_cache
+from itertools import product
+from math import inf
+
+from board import Board
+
+class AI():
+    def __init__(self, player_id, difficulty, num_players, winning_row_length):
+        self.difficulty = difficulty
+        self.p_id = player_id
+        self.num_players = num_players
+        self.winning_row_length = winning_row_length
+
+    def get_move(self, board):
+        if self.difficulty == 1:
+            return self.get_random_move(board)
+        else:
+            return self.get_optimal_move(board)
+
+    def get_random_move(self, board):
+        while (True):
+            move = [randrange(board.size), randrange(board.size)]
+            if not board.is_point_marked(move):
+                return move
+
+    def get_possible_next_states(self, board, curr_player):
+        possible_next_states = [None for x, y in product(range(board.size), range(board.size))]
+        for x, y in product(range(board.size), range(board.size)):
+            if not board.is_point_marked([x, y]):
+                temp = board.from_board(board)
+                temp.add_mark(x, y, curr_player)
+                possible_next_states[board.hash(x, y)] = temp
+        return possible_next_states
+
+    def get_optimal_move(self, board):
+        possible_next_states = self.get_possible_next_states(board, self.p_id)
+        scores = [self.score_state(state, prev_player=self.p_id)
+                  if state is not None
+                  else (- inf)
+                  for state in possible_next_states]
+        print(scores)
+        return board.unhash(max(range(len(scores)), key=scores.__getitem__))
+
+    def score_state(self, board, prev_player, depth = 0):
+        winner = board.get_player_with_score(self.winning_row_length)
+        next_player = max((prev_player + 1) % (self.num_players + 1),1)
+        if winner == -1:
+            return 0
+        elif winner > 0:
+            return depth + 1 if winner == self.p_id else -(depth + 1)
+        possible_next_states = self.get_possible_next_states(board, next_player)
+        if prev_player != self.p_id:
+            return max([self.score_state(state, next_player, depth + 1)
+                   if state
+                   else (- inf)
+                   for state in possible_next_states])
+        else:
+            return min([self.score_state(state, next_player, depth + 1)
+                   if state
+                   else inf
+                   for state in possible_next_states])
---
+++ game.py
@@ -0,0 +1,41 @@
+from time import sleep
+from collections import deque
+
+from ai import AI
+from player import Player
+from board import Board
+
+class GeneralizedTicTacToe():
+    def __init__(self, size, num_players, num_ai_players, ai_difficulty, winning_row_length):
+        self.board = Board(size=size)
+        self.size = size
+        self.num_players = num_players
+        self.num_ai_players = num_ai_players
+        self.ai_difficulty = ai_difficulty
+        self.winning_row_length = winning_row_length
+
+    def start(self):
+        players = []
+        for i in range(self.num_players):
+            players.append(Player(player_id=i+1))
+        for i in range(self.num_ai_players):
+            players.append(AI(player_id=self.num_players+i+1,
+                              difficulty=self.ai_difficulty,
+                              num_players=self.num_players + self.num_ai_players,
+                              winning_row_length=self.winning_row_length))
+
+        self.board.display()
+        turn = 0
+        while True:
+            x, y = players[turn].get_move(self.board)
+            self.board.add_mark(x, y, turn+1)
+            self.board.display()
+            winner = self.board.get_player_with_score(self.winning_row_length)
+            if winner == -1:
+                print('Draw!')
+                break
+            elif winner != 0:
+                print('Player ' + str(winner) + ' is the winner!')
+                break
+            turn = (turn + 1) % len(players)
+            sleep(1)
